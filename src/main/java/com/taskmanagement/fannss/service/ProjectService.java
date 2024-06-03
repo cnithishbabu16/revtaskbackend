72b64b31e3ac6707178083113524d7b363b2c10e
cnithishbabu16 <cnithishbabu14@gmail.com>
2024-06-03 07:59:47 +0530

    rev

--- src/main/java/com/taskmanagement/fannss/service/ProjectService.java
+++ src/main/java/com/taskmanagement/fannss/service/ProjectService.java
@@ -44,7 +44,7 @@ public class ProjectService {
 
         // Set client and project manager using IDs from the DTO
         Client client = clientRepository.findById(createProjectDTO.getClientId()).orElse(null);
-        User projectManager = userRepository.findById(createProjectDTO.getProjectManagerId()).orElse(null);
+        User projectManager =  User projectManager = userRepository.findById(userId).orElse(null);
 
         project.setClient(client);
         project.setProjectManager(projectManager);
